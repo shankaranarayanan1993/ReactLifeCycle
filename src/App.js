5b42c5c5e6d169ebc0752d59ada85d6692fd16a7
shankar <shankaranarayanan712@gmail.com>
2018-12-07 22:34:27 +0530

    Life Cycle Methods

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Counter from "./lifecycle";
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            mount : false,
+            ignoreProps : 0,
+            generateSeed: 40
+        }
+        this.mount = () => this.setState({mount:true});
+        this.unMount = () => this.setState({mount:false});
+        this.ignoreProps = () => this.setState({ignoreProps:Math.random()});
+        this.generateSeed = () => this.setState({generateSeed: Number.parseInt(Math.random() * 100)});
+    }
   render() {
     return (
       <div className="App">
+          <div>
+              <button onClick={this.mount} disabled={this.state.mount}>Mount </button>
+              <br/>
+              <button onClick={this.unMount} disabled={!this.state.mount}>Un Mount </button>
+              <br/>
+              <button onClick={this.generateSeed} >Ignore Props</button>
+          </div>
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <br/>
+            <button onClick={this.generateSeed} >Generate Seed</button>
+            {this.state.mount ?
+                <Counter
+                ignoreProps = {this.state.ignoreProps}
+                generateSeed = {this.state.generateSeed}
+                /> : null }
         </header>
       </div>
     );
---
+++ src/lifecycle.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+
+const ErrorComponent = () =>  (<div> Error : {this.errorUnhandled}</div>)
+export default class Counter extends React.Component {
+    constructor(props){
+        console.log("1 Constructor");
+        console.log("--------------------------");
+        super(props); // This sets this.props to props
+
+        this.state = {
+            counter : 0,
+            generateSeed: 0
+        };
+        this.increment = ()=> this.setState({counter: this.state.counter +1});
+        this.decrement = ()=> this.setState({counter: this.state.counter -1});
+    }
+
+    static getDerivedStateFromProps(props,state){// This will map props to state
+        console.log("3 Get Derived State to props");
+        console.log("--------------------------");
+       if(props.generateSeed && state.generateSeed !== props.generateSeed){
+           return {
+               generateSeed : props.generateSeed,
+               counter: props.generateSeed
+           }
+       }
+       return null;
+
+    }
+
+
+    componentDidMount(){
+        console.log("4 Component Did Mount");
+        console.log("--------------------------");
+    }
+
+    shouldComponentUpdate(nextProps , nextState){ //  returns true by default
+        if(nextProps.ignoreProps &&
+           this.props.ignoreProps !== nextProps.ignoreProps){
+            console.log('ignoreProps',this.props.ignoreProps);
+            return false;
+        }
+        return true;
+        console.log("5 Should Component Update");
+        console.log("--------------------------");
+    }
+
+    render() {
+        console.log("2 Render");
+        console.log("--------------------------");
+        return (
+        <div>
+            <div>
+                <button onClick={this.increment}> Increment</button>
+             <br/>
+                <button onClick={this.decrement}> Decrement</button>
+            </div>
+
+            <div className="Counter">
+                 Counter : {this.state.counter}
+              {/*  <ErrorComponent/>*/}
+            </div>
+
+        </div>
+        )
+    }
+
+    componentDidUpdate(prevProps, prevState, snapShot){
+       /* console.log("Prev Props", prevProps);
+        console.log("Prev State", prevState);
+        console.log("Snap Shot", snapShot);*/
+        console.log("7 Component Did Update");
+        console.log("--------------------------");
+    }
+
+    componentWillUnmount(){
+        console.log("7 Component Will Unmount");
+        console.log("--------------------------");
+    }
+    componentDidCatch(){
+        // This will catch the error in a particular component and will avaod whole application crash, but the component
+        // that has error willthroe erroe
+        console.log("7 Component Did catch Error");
+        console.log("--------------------------");
+    }
+}
